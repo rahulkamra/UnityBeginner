bc6e384a4c7b58bae839728190b5c043a0e69eef
rahulkamra <rahulkamra@gmail.com>
2016-08-13 15:14:40 +0400

    Added Player Pref Saving
    Added Player Pref Saving

---
+++ Assets/SavingGame/PlayerPrefSaving/BootScriptPlayerPref.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BootScriptPlayerPref : MonoBehaviour
+{
+
+    private Button btnSaveText;
+    private InputField inputField;
+    private Button btnGetText;
+    private Text outputField;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        btnSaveText = GameObject.Find("btnSaveText").GetComponent<Button>(); ;
+        btnSaveText.onClick.AddListener(OnClick);
+
+        btnGetText = GameObject.Find("btnGetText").GetComponent<Button>();
+        btnGetText.onClick.AddListener(OnReadText);
+
+        inputField = GameObject.Find("txtInput").GetComponent<InputField>();
+        outputField = GameObject.Find("output").GetComponent<Text>();
+        OnReadText();
+
+    }
+
+    public void OnReadText()
+    {
+        string value = PlayerPrefs.GetString("key");
+        Debug.Log(outputField);
+        if(value != null)
+            outputField.text = value;
+    }
+
+    public void OnClick()
+    {
+        string currentText = inputField.text;
+        PlayerPrefs.SetString("key", currentText);
+        PlayerPrefs.Save();
+        //save is important to avoid crash by default unity save on exit
+    }
+	
+	
+}

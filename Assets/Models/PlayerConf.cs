63ba672fb36fb54076b7dec0e37faf449db7538b
rahulkamra <rahulkamra@gmail.com>
2016-08-14 17:07:08 +0400

    Added Conf file,  need to load the scriptable object and conf on the runtime.

---
+++ Assets/Models/PlayerConf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Models
+{
+    public class PlayerConf:ScriptableObject
+    {
+        public string NAME;
+        public int HP = 100;
+        public int MAX_ATTACK = 200;
+
+    }
+}
---
+++ Assets/SavingGame/ScriptableObjects/BootScriptScriptableObjects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BootScriptScriptableObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        string assetPath = Application.dataPath + "/SavingGame/ScriptableObjects/Instances/EnemyA.asset";
+        Object asset = Resources.Load(assetPath);
+        Debug.Log(asset);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

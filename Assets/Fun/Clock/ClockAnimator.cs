da9e2fd4038988c1cd6910f7458a6efc44ade2f6
rahulkamra <rahulkamra@gmail.com>
2016-11-20 16:13:55 +0400

    Added MultiThreading Example and Clock from Catlike coding.

---
+++ Assets/Fun/Clock/ClockAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Transform seconds;
+    public Transform minutes;
+    public Transform hours;
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        DateTime time = DateTime.Now;
+        float seconds = time.Second;
+
+        double secondsAngle = - (time.Second / 60.0f) * 360;
+        double minutesAngle = -(time.Minute / 60.0f) * 360;
+        double hoursAngle = -(time.Hour / 12.0f) * 360;
+
+
+        this.seconds.transform.rotation  = Quaternion.Euler(new Vector3(0, 0, (float)secondsAngle));
+        this.minutes.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)minutesAngle));
+        this.hours.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)hoursAngle));
+ 
+    }
+}
---
+++ Assets/MultiThreading/ThreadFunctionMapper.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class ThreadFunctionMapper : MonoBehaviour
+{
+
+    private static int count = 0;
+    private static string result = "";
+    public void ExecuteSameThread()
+    {
+        BigForLoop();
+    }
+
+    public void ExecuteDifferentThread()
+    {
+        Thread thread = new Thread(onThreadStart);
+        thread.Start();
+        //Debug.Log("Execution Ended");
+    }
+
+    public void onThreadStart()
+    {
+        getBigData();
+    }
+
+
+    public void BigForLoop()
+    {
+        for (int idx = 0; idx < 1000; idx++)
+        {
+            count++;
+            Debug.Log("Debug Output " + count);
+        }
+    }
+
+    public string getBigData()
+    {
+        string strReturn = "";
+
+        for (int idx = 0; idx< 10000; idx++)
+        {
+            strReturn += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        }
+        Debug.Log("Execution Completed");
+        ThreadFunctionMapper.result = strReturn;
+        return strReturn;
+    }
+}

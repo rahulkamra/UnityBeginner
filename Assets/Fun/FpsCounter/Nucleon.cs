8f92024d766cba3e5db2c4ea52c51a63f2029177
rahulkamra <rahulkamra@gmail.com>
2016-11-21 17:08:09 +0400

    started fps counter

---
+++ Assets/Fun/FpsCounter/Nucleon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+    Rigidbody body;
+    // Use this for initialization
+    public float forceMultiplier;
+	void Awake ()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        body.AddForce(-transform.localPosition * forceMultiplier);
+	}
+}
---
+++ Assets/Fun/FpsCounter/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public Nucleon[] neucleons;
+    public float spawnTime;
+    public float distance;
+    
+
+    float timePassed = 0;
+    float lastSpawnTime = 0;
+    void FixedUpdate ()
+    {
+        this.timePassed =   this.timePassed + Time.deltaTime;
+        if(timePassed - lastSpawnTime > spawnTime)
+        {
+            lastSpawnTime = timePassed;
+            spawnAnObject();
+        }
+	}
+    
+    private void spawnAnObject()
+    {
+        Nucleon prefab = neucleons[Random.Range(0, neucleons.Length)];
+        Nucleon obj = Instantiate<Nucleon>(prefab);//Instantiate(prefab);
+        
+        obj.transform.localPosition = Random.onUnitSphere * distance;
+        
+    }
+
+
+}

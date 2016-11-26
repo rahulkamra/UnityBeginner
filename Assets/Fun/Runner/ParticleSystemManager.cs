f4585ec237fa3d30d35d27461272997351afb3c7
rahulkamra <rahulkamra@gmail.com>
2016-11-26 22:46:51 +0400

    added particles and the tutorial is ended.

---
+++ Assets/Fun/Runner/ParticleSystemManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemManager : MonoBehaviour {
+
+	// Use this for initialization
+
+    public ParticleSystem[] ParticleSystems;
+
+	void Start ()
+    {
+	    GameManager.EventDispatcher.AddEventListner(RunnerEvents.EVENT_GAME_START,OnGameStart);
+        GameManager.EventDispatcher.AddEventListner(RunnerEvents.EVENT_GAME_END, OnGameEnd);
+        OnGameEnd(null);
+    }
+
+
+    public void OnGameStart(Event Event)
+    {
+        for(int idx = 0 ; idx < ParticleSystems.Length ; idx++)
+        {
+            ParticleSystems[idx].Clear();;
+            ParticleSystems[idx].enableEmission = true;
+        }
+    }
+
+    public void OnGameEnd(Event Event)
+    { 
+        for(int idx = 0; idx<ParticleSystems.Length ; idx++)
+        {
+          ParticleSystems[idx].enableEmission = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}

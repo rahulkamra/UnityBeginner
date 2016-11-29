587c4629d78ca66164f3bc19a25ca94f746b1fea
rahulkamra <rahulkamra@gmail.com>
2016-11-29 14:35:07 +0400

    Made the star with vertex color.

--- Assets/Fun/Star/Star.cs
+++ Assets/Fun/Star/Star.cs
@@ -4,43 +4,61 @@ using System.Collections;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Star : MonoBehaviour {
 
-    public int NumVertices;
+    
+    public int Frequency;
+    public ColorPointModel Center;
+    public ColorPointModel[] Points;
+
     private Mesh Mesh;
 
     private Vector3[] vertices;
     private int[] indicies;
+    private Color[] colors;
 
     private void Start()
     {
         this.Mesh = new Mesh();
         this.GetComponent<MeshFilter>().mesh = this.Mesh;
         this.Mesh.name = "Star";
+        int NumVertices = Points.Length * Frequency;
 
-        vertices = new Vector3[NumVertices + 1];
-
+        if (NumVertices < 3)
+            return;
+        
+        
         //
         float numDegress = -360f / NumVertices;
 
+        vertices = new Vector3[NumVertices + 1];
         indicies = new int[3 * NumVertices];
+        colors = new Color[NumVertices + 1];
+
 
         //the first one is in the middle
-        vertices[0] = new Vector3(0, 0, 0);
+        vertices[0] = Center.Position;
+        colors[0] = Center.Color;
 
         //if vertices are 0 , 1, 2,3
         //indics are 0,1,2    0,2,3    0,3,4   0,4,1
-        for (int vdx = 1 , idx = 1; vdx < vertices.Length; vdx++,idx +=3)
-        {
-            vertices[vdx] = Quaternion.Euler(0f, 0f, numDegress * (vdx - 1)) * Vector3.up;
 
-           // indicies[3 * vdx - 3] = 0;//we can skip this if we want to
-            indicies[idx] = vdx;
-            indicies[idx+1] = vdx + 1;
+        for (int fdx = 0,vdx = 1, idx = 1 ; fdx < Frequency; fdx++)
+        {
+            for ( int pdx = 0; pdx < Points.Length; pdx++, vdx++, idx += 3)
+            {
+
+                vertices[vdx] = Quaternion.Euler(0f, 0f, numDegress * (vdx - 1)) * Points[pdx].Position;
+                colors[vdx] = Points[pdx].Color;
+                // indicies[3 * vdx - 3] = 0;//we can skip this if we want to
+                indicies[idx] = vdx;
+                indicies[idx + 1] = vdx + 1;
+            }
         }
+       
         
         indicies[indicies.Length - 1] = 1;
         this.Mesh.vertices = vertices;
         this.Mesh.triangles  = indicies;
-
+        this.Mesh.colors = colors;
     }
 
 }
--- Assets/Fun/Star/VertexColorShader.shader
+++ Assets/Fun/Star/VertexColorShader.shader
@@ -35,19 +35,21 @@
 			struct v2f
 			{
 				float4 vertex : SV_POSITION;
+				fixed4 color : COLOR;
 			};
 
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.color = v.color;
 				return o;
 			}
 			
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
-				return fixed4(1,1,1,1);
+				return i.color;
 			}
 			ENDCG
 		}

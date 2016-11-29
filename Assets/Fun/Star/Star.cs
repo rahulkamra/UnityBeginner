da51af4810508457c2519cc45a37464d3e402b5c
rahulkamra <rahulkamra@gmail.com>
2016-11-29 14:01:19 +0400

    added custom shader and star scene.

---
+++ Assets/Fun/Star/Star.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Star : MonoBehaviour {
+
+    public int NumVertices;
+    private Mesh Mesh;
+
+    private Vector3[] vertices;
+    private int[] indicies;
+
+    private void Start()
+    {
+        this.Mesh = new Mesh();
+        this.GetComponent<MeshFilter>().mesh = this.Mesh;
+        this.Mesh.name = "Star";
+
+        vertices = new Vector3[NumVertices + 1];
+
+        //
+        float numDegress = -360f / NumVertices;
+
+        indicies = new int[3 * NumVertices];
+
+        //the first one is in the middle
+        vertices[0] = new Vector3(0, 0, 0);
+
+        //if vertices are 0 , 1, 2,3
+        //indics are 0,1,2    0,2,3    0,3,4   0,4,1
+        for (int vdx = 1 , idx = 1; vdx < vertices.Length; vdx++,idx +=3)
+        {
+            vertices[vdx] = Quaternion.Euler(0f, 0f, numDegress * (vdx - 1)) * Vector3.up;
+
+           // indicies[3 * vdx - 3] = 0;//we can skip this if we want to
+            indicies[idx] = vdx;
+            indicies[idx+1] = vdx + 1;
+        }
+        
+        indicies[indicies.Length - 1] = 1;
+        this.Mesh.vertices = vertices;
+        this.Mesh.triangles  = indicies;
+
+    }
+
+}
---
+++ Assets/Fun/Star/VertexColorShader.shader
@@ -0,0 +1,55 @@
+﻿Shader "Temp/VertexColorShader"
+{
+	Properties
+	{
+		
+	}
+	SubShader
+	{
+		Tags
+	{
+		"Queue" = "Transparent"
+		"IgnoreProjector" = "True"
+		"RenderType" = "Transparent"
+		"PreviewType" = "Plane"
+		"CanUseSpriteAtlas" = "True"
+	}
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			uniform float4 _Color;
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				fixed4 color : COLOR;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				return o;
+			}
+			
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return fixed4(1,1,1,1);
+			}
+			ENDCG
+		}
+	}
+}

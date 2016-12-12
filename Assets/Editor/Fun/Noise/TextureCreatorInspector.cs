59df0119bb28f601fbe1d5c0fcebe8c6c01659dd
rahulkamra <rahulkamra@gmail.com>
2016-12-12 17:39:01 +0400

    Worked on some more tutorial.

---
+++ Assets/Editor/Fun/Noise/TextureCreatorInspector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TextureCreator))]
+public class TextureCreatorInspector : Editor
+{
+
+    private TextureCreator creator;
+    public override void OnInspectorGUI()
+    {
+        creator =  this.target as TextureCreator;
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if(EditorGUI.EndChangeCheck() && Application.isPlaying)
+        {
+            creator.FillTexture();
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = this.target as TextureCreator;
+        Undo.undoRedoPerformed += RefreshTexture;
+    }
+
+    private void OnDisable()
+    {
+        creator = this.target as TextureCreator;
+        Undo.undoRedoPerformed -= RefreshTexture;
+    }
+
+    private void RefreshTexture()
+    {
+        if (Application.isPlaying)
+            creator.FillTexture();
+    }
+
+}
--- Assets/Fun/Noise/TextureCreator.cs
+++ Assets/Fun/Noise/TextureCreator.cs
@@ -1,34 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class TextureCreator : MonoBehaviour {
 
 	// Use this for initialization
+    [Range(2,1024)]
     public int Resolution = 256;
 
     private Texture2D texture;
 	void OnEnable()
     {
-        texture = new Texture2D(Resolution, Resolution, TextureFormat.RGB24,true);
-	    texture.name = "Main Texture";
-	    GetComponent<MeshRenderer>().material.mainTexture = texture;
-	    fillTexture();
+        if(texture == null)
+        {
+            texture = new Texture2D(Resolution, Resolution, TextureFormat.RGB24, false);
+            texture.name = "Main Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 9;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
+
+        FillTexture();
     }
 
-
-    void fillTexture()
+    public void FillTexture()
     {
+        if(texture.width != Resolution)
+        {
+            texture.Resize(Resolution, Resolution);
+        }
+
+        float stepSize = 1f / Resolution;
         for (int idx = 0; idx < Resolution; idx++)
         {
             for (int jdx = 0; jdx < Resolution; jdx++)
             {
-                texture.SetPixel(idx,jdx,new Color((float)idx/Resolution, (float)jdx / Resolution, 0f));
+                float xVal = (idx + 0.5f) * stepSize % 0.1f;
+                float yVal = (jdx + 0.5f) * stepSize % 0.1f;
+
+                texture.SetPixel(idx, jdx, new Color(xVal, yVal, 0f) * 10f) ;
             }
         }
         texture.Apply();
     }
-    // Update is called once per frame
-    void Update () {
-	
-	}
+    
+    
 }

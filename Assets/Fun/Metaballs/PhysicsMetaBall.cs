4f6b3b8c4bcf714679f6e13407374a3d6f5cadac
rahulkamra <rahulkamra@gmail.com>
2017-01-04 17:09:58 +0400

    Added The physics scene.

---
+++ Assets/Fun/Metaballs/PhysicsMetaBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsMetaBallWorld : GPUMetaBallCreator
+{
+    
+    public GameObject Parent;
+
+
+    protected override void onStart()
+    {
+        NumBalls = Parent.transform.childCount;
+        base.onStart();
+    }
+
+     void Update()
+    {
+        for (int idx = 0; idx < Parent.transform.childCount; idx++)
+        {
+            ((Metaball)balls[idx]).Position = Parent.transform.GetChild(idx).transform.position;
+        }
+        
+        OnUpdate();
+        
+    }
+    
+}
+
+
+
+
+
+

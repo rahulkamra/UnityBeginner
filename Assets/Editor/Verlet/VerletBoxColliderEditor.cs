12ab05951d720c924e230957bc2107d525d8c61f
rahulkamra <rahulkamra@gmail.com>
2017-01-08 13:36:56 +0400

    Started working on the Verlet Integration

---
+++ Assets/Editor/Verlet/VerletBoxColliderEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VerletBoxCollider))]
+class VerletBoxColliderEditor : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // Take out this if statement to set the value using setter when ever you change it in the inspector.
+        // But then it gets called a couple of times when ever inspector updates
+        // By having a button, you can control when the value goes through the setter and getter, your self.
+        if (GUILayout.Button("Use setters/getters"))
+        {
+            if (target.GetType() == typeof(VerletBoxCollider))
+            {
+                VerletBoxCollider getterSetter = (VerletBoxCollider)target;
+                getterSetter.width = getterSetter.width;
+                DrawLines(getterSetter);
+                SceneView.RepaintAll();
+            }
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        VerletBoxCollider collider = this.target as VerletBoxCollider;
+        if (collider == null)
+            return;
+
+        DrawLines(collider);
+    }
+
+
+    private void DrawLines(VerletBoxCollider collider)
+    {
+        for (int idx = 0; idx < collider.Points.Length - 1; idx++)
+        {
+            Handles.DrawLine(collider.transform.TransformPoint(collider.Points[idx]), collider.transform.TransformPoint(collider.Points[idx + 1]));
+        }
+        Handles.DrawLine(collider.transform.TransformPoint(collider.Points[collider.Points.Length - 1]), collider.transform.TransformPoint(collider.Points[0]));
+    }
+
+
+}
+
---
+++ Assets/Verlet/VerletBoxCollider.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class VerletBoxCollider : MonoBehaviour
+{
+    private void Start()
+    {
+        initPoints();
+    }
+    [SerializeField]
+    private float _width = 1;
+    
+    public float width
+    {
+        get
+        {
+           return  _width;
+        }
+
+        set
+        {
+            this._width = value;
+            initPoints();
+        }
+    }
+
+    [SerializeField]
+    private float _height = 1;
+    
+    public float height
+    {
+        get
+        {
+            return _height;
+        }
+
+        set
+        {
+            this._height = value;
+            initPoints();
+        }
+    }
+
+    [HideInInspector]
+    public Vector2[] Points;
+
+    private void initPoints()
+    {
+        Points = new Vector2[4];
+        Points[0] = new Vector2(- _width / 2f, - _height / 2f);
+        Points[1] = new Vector2(_width / 2f,- _height / 2f);
+        Points[2] = new Vector2(_width / 2f, _height / 2f);
+        Points[3] = new Vector2(- _width / 2f,_height / 2f);
+       
+    }
+    
+}
